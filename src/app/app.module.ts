d09a706014d6e1b9f86c16f3e81f00c893e2210a
YotamAk <yotam023@gmail.com>
2019-03-12 16:34:52 +0200

    update list from API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CinemaService } from '../common/services/cinema.service';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MovieComponent } from './main/movie/movie.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MovieComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CinemaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
